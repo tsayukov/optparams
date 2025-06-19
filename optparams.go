74c0bd605560b81adc7667223b826a5e00767752
Pavel Tsayukov <p.tsayukov@gmail.com>
2025-06-19 14:02:00 +0300

    Add ErrFailFast sentinel error

--- optparams.go
+++ optparams.go
@@ -1,4 +1,12 @@
 package optparams
 
+import (
+	"errors"
+)
+
 // Func should initialize some fields in the passed receiver.
 type Func[T any] func(receiver *T) error
+
+// ErrFailFast indicates that a failure in the [Func] call causes the [Apply]
+// call to terminate early.
+var ErrFailFast = errors.New("fail fast")
