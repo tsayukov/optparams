7ff565804a7900f3068593aceee74210b35b20c3
Pavel Tsayukov <p.tsayukov@gmail.com>
2025-06-24 00:36:39 +0300

    Add tests

---
+++ optparams_test.go
@@ -0,0 +1,236 @@
+// This file is licensed under the terms of the MIT License (see LICENSE file)
+// Copyright (c) 2025 Pavel Tsayukov
+
+package optparams
+
+import (
+	"errors"
+	"fmt"
+	"math/rand/v2"
+	"slices"
+	"strconv"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_Apply(t *testing.T) {
+	type mockReceiver struct {
+		values []rune
+	}
+
+	newMockReceiver := func(count ...int) *mockReceiver {
+		if len(count) == 0 {
+			return &mockReceiver{}
+		}
+
+		return &mockReceiver{values: make([]rune, count[0])}
+	}
+
+	newMockReceiverOf := func(values ...rune) *mockReceiver {
+		return &mockReceiver{values: values}
+	}
+
+	newOpt := func(pos int, value rune, err ...error) Func[mockReceiver] {
+		return func(mr *mockReceiver) error {
+			if len(err) > 0 && err[0] != nil {
+				return err[0]
+			}
+
+			mr.values[pos] = value
+
+			return nil
+		}
+	}
+
+	errMocks := func() []error {
+		const size = 5
+
+		errs := make([]error, 0, size)
+		for i := 0; i < size; i++ {
+			errs = append(errs, errors.New(strconv.Itoa(i)))
+		}
+
+		return errs
+	}()
+
+	type testCase struct {
+		name     string
+		receiver *mockReceiver
+		want     *mockReceiver
+		opts     []Func[mockReceiver]
+		wantErr  error
+	}
+
+	tests := []testCase{
+		{
+			name:    "nil receiver",
+			wantErr: fmt.Errorf("receiver %T is nil", mockReceiver{}),
+		},
+		{
+			name:     "no opts",
+			receiver: newMockReceiver(),
+			want:     newMockReceiver(),
+		},
+		{
+			name:     "one successful opt",
+			receiver: newMockReceiver(1),
+			want:     newMockReceiverOf('a'),
+			opts: []Func[mockReceiver]{
+				newOpt(0, 'a'),
+			},
+		},
+		{
+			name:     "one failed opt",
+			receiver: newMockReceiver(),
+			opts: []Func[mockReceiver]{
+				newOpt(0, 'x', errMocks[0]),
+			},
+			wantErr: errMocks[0],
+		},
+		{
+			name:     "multiple opts",
+			receiver: newMockReceiver(7),
+			want:     newMockReceiverOf('a', 'b', 'c', 'd', 'e', 'f', 'g'),
+			opts: func() []Func[mockReceiver] {
+				opts := []Func[mockReceiver]{
+					newOpt(0, 'a'),
+					newOpt(1, 'b'),
+					newOpt(2, 'c'),
+					newOpt(3, 'd'),
+					newOpt(4, 'e'),
+					newOpt(5, 'f'),
+					newOpt(6, 'g'),
+				}
+
+				rand.Shuffle(len(opts), func(i, j int) {
+					opts[i], opts[j] = opts[j], opts[i]
+				})
+
+				return opts
+			}(),
+		},
+		{
+			name:     "multiple opts with first fast fail error",
+			receiver: newMockReceiver(3),
+			want:     newMockReceiverOf(0, 0, 0),
+			opts: []Func[mockReceiver]{
+				newOpt(2, 'x', ErrFailFast),
+				newOpt(1, 'b'),
+				newOpt(0, 'a'),
+			},
+			wantErr: ErrFailFast,
+		},
+		{
+			name:     "partial init with fast fail error",
+			receiver: newMockReceiver(3),
+			want:     newMockReceiverOf(0, 'b', 0),
+			opts: []Func[mockReceiver]{
+				newOpt(1, 'b'),
+				newOpt(2, 'x', ErrFailFast),
+				newOpt(0, 'a'),
+			},
+			wantErr: ErrFailFast,
+		},
+		{
+			name:     "wrapped fail fast error",
+			receiver: newMockReceiver(3),
+			want:     newMockReceiverOf(0, 0, 0),
+			opts: []Func[mockReceiver]{
+				newOpt(0, 'x', fmt.Errorf("err: %w", ErrFailFast)),
+				newOpt(1, 'b'),
+				newOpt(1, 'c'),
+			},
+			wantErr: fmt.Errorf("err: %w", ErrFailFast),
+		},
+		{
+			name:     "multiple opts with one error",
+			receiver: newMockReceiver(3),
+			want:     newMockReceiverOf('a', 0, 'c'),
+			opts: []Func[mockReceiver]{
+				newOpt(0, 'a'),
+				newOpt(1, 'x', errMocks[0]),
+				newOpt(2, 'c'),
+			},
+			wantErr: errMocks[0],
+		},
+		{
+			name:     "multiple opts with a few errors",
+			receiver: newMockReceiver(6),
+			want:     newMockReceiverOf('a', 0, 'c', 0, 0, 'f'),
+			opts: []Func[mockReceiver]{
+				newOpt(0, 'a'),
+				newOpt(1, 'x', errMocks[0]),
+				newOpt(2, 'c'),
+				newOpt(3, 'x', errMocks[1]),
+				newOpt(4, 'x', errMocks[2]),
+				newOpt(5, 'f'),
+			},
+			wantErr: errors.Join(errMocks[0], errMocks[1], errMocks[2]),
+		},
+		{
+			name:     "multiple opts with all errors",
+			receiver: newMockReceiver(len(errMocks)),
+			want:     newMockReceiverOf(slices.Repeat([]rune{0}, len(errMocks))...),
+			opts: func() []Func[mockReceiver] {
+				opts := make([]Func[mockReceiver], 0, len(errMocks))
+				for i := range errMocks {
+					opts = append(opts, newOpt(i, 'x', errMocks[i]))
+				}
+
+				return opts
+			}(),
+			wantErr: errors.Join(errMocks...),
+		},
+		{
+			name:     "nil field",
+			receiver: newMockReceiver(),
+			opts: []Func[mockReceiver]{
+				Default[mockReceiver](nil, 'a'),
+			},
+			wantErr: fmt.Errorf(
+				"pointer %T to field in receiver %T is nil",
+				rune(0), mockReceiver{},
+			),
+		},
+		func() testCase {
+			mr := newMockReceiver(1)
+			return testCase{
+				name:     "no opt + default",
+				receiver: mr,
+				want:     newMockReceiverOf('a'),
+				opts: []Func[mockReceiver]{
+					Default[mockReceiver](&mr.values[0], 'a'),
+				},
+			}
+		}(),
+		func() testCase {
+			mr := newMockReceiver(1)
+			return testCase{
+				name:     "opt + default",
+				receiver: mr,
+				want:     newMockReceiverOf('a'),
+				opts: []Func[mockReceiver]{
+					newOpt(0, 'a'),
+					Default[mockReceiver](&mr.values[0], 'a'),
+				},
+			}
+		}(),
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			err := Apply(tt.receiver, tt.opts...)
+
+			if tt.wantErr != nil {
+				assert.EqualError(t, err, tt.wantErr.Error())
+			} else {
+				assert.NoError(t, err)
+			}
+
+			assert.Equal(t, tt.want, tt.receiver)
+		})
+	}
+}
