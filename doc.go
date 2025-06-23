cccdcc6016743e0fabe7768231b36077967215b9
Pavel Tsayukov <p.tsayukov@gmail.com>
2025-06-23 15:13:03 +0300

    Add doc.go

---
+++ doc.go
@@ -0,0 +1,6 @@
+// This file is licensed under the terms of the MIT License (see LICENSE file)
+// Copyright (c) 2025 Pavel Tsayukov
+
+// Package optparams helps create functions with optional parameters, including
+// those with or without default values.
+package optparams
