de5a4de72c30937c7ad3f07d19af763620ba9eae
Pavel Tsayukov <p.tsayukov@gmail.com>
2025-06-25 14:47:35 +0300

    Add author's email in headers

--- doc.go
+++ doc.go
@@ -1,5 +1,5 @@
 // This file is licensed under the terms of the MIT License (see LICENSE file)
-// Copyright (c) 2025 Pavel Tsayukov
+// Copyright (c) 2025 Pavel Tsayukov p.tsayukov@gmail.com
 
 // Package optparams helps create functions with optional parameters, including
 // those with or without default values.
